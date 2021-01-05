d8dae112671905904a7da7538965f1446bcc6574
Aleksandr Kruhlov <alkruglov777@gmail.com>
2021-01-05 19:01:07 +0200

    Make refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,77 +9,14 @@ import {ServerService} from './server.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  question: Question = this.createNewQuestion();
+  question: Question = AppComponent.createNewQuestion();
   questions: Question[] = [];
 
   constructor(private serverService: ServerService) {
     this.findAllQuestions();
   }
 
-  private findAllQuestions(): void {
-    this.serverService.findAllQuestions()
-      .subscribe((questions: Question[]) => this.questions = questions);
-  }
-
-  addQuestion(): void {
-    this.question.answerOptions = this.question.answerOptions.filter((answerOption) => answerOption.answerOptionText !== '');
-    this.serverService.saveQuestion(this.question)
-      .subscribe(() => this.findAllQuestions());
-  }
-
-  trackByFn(index: any, item: any): any {
-    return item.id;
-  }
-
-  getAnswerOptionCount(): number {
-    let emptyAnswerOptionCount = 0;
-    this.question.answerOptions.forEach((answerOption) => {
-      if (answerOption.answerOptionText === '') {
-        emptyAnswerOptionCount++;
-      }
-    });
-    return this.question.answerOptions.length - emptyAnswerOptionCount;
-  }
-
-  addNewAnswerOption(): void {
-    this.question.answerOptions.push({
-      id: null,
-      answerOptionText: ''
-    });
-  }
-
-  deleteAnswerOption(index: number): void {
-    this.question.answerOptions.splice(index, 1);
-    if (!this.question.answerOptions.length) {
-      this.addNewAnswerOption();
-    }
-  }
-
-  isAnswerOptionLastAndNonEmpty(answerOption: AnswerOption, answerOptionIndex: number): boolean {
-    return answerOption.answerOptionText !== '' && answerOptionIndex === this.question.answerOptions.length - 1;
-  }
-
-  saveQuestion(): void {
-    if (confirm('Ok')) {
-      console.log(this.question);
-      this.addQuestion();
-      this.clearAllQuestion();
-    }
-  }
-
-  clearAllQuestion(): void {
-    this.question = this.createNewQuestion();
-  }
-
-  isDisabled(): boolean {
-    return this.question.questionText === '' || this.allAnswerOptionsIsEmpty();
-  }
-
-  allAnswerOptionsIsEmpty(): boolean {
-    return !this.question.answerOptions.find(answerOption => answerOption.answerOptionText !== '');
-  }
-
-  createNewQuestion(): Question {
+  private static createNewQuestion(): Question {
     return {
       id: null,
       questionText: '',
@@ -93,5 +30,61 @@ export class AppComponent {
 
   clearQuestionText(): void {
     this.question.questionText = '';
+  }
+
+  saveQuestion(): void {
+    if (confirm('Ok')) {
+      this.question.answerOptions = this.searchNonEmptyAnswerOptions();
+      this.serverService.saveQuestion(this.question)
+        .subscribe(() => this.findAllQuestions());
+      this.renewQuestion();
+    }
+  }
+
+  renewQuestion(): void {
+    this.question = AppComponent.createNewQuestion();
+  }
+
+  private findAllQuestions(): void {
+    this.serverService.findAllQuestions()
+      .subscribe((questions: Question[]) => this.questions = questions);
+  }
+
+  calculateAnswerOptionCount(): number {
+    return this.searchNonEmptyAnswerOptions().length;
+  }
+
+  private searchNonEmptyAnswerOptions(): AnswerOption[] {
+    return this.question.answerOptions.filter((answerOption) => answerOption.answerOptionText !== '');
+  }
+
+  addNewAnswerOption(): void {
+    this.question.answerOptions.push({
+      id: null,
+      answerOptionText: ''
+    });
+  }
+
+  trackByFn(index: any, item: any): any {
+    return item.id;
+  }
+
+  removeAnswerOption(index: number): void {
+    this.question.answerOptions.splice(index, 1);
+    if (!this.question.answerOptions.length) {
+      this.addNewAnswerOption();
+    }
+  }
+
+  isAnswerOptionLastAndNonEmpty(answerOption: AnswerOption, answerOptionIndex: number): boolean {
+    return answerOption.answerOptionText !== '' && answerOptionIndex === this.question.answerOptions.length - 1;
+  }
+
+  private areAllAnswerOptionsEmpty(): boolean {
+    return !this.question.answerOptions.find(answerOption => answerOption.answerOptionText !== '');
+  }
+
+  isSaveButtonDisabled(): boolean {
+    return this.question.questionText === '' || this.areAllAnswerOptionsEmpty();
   }
 }
